37aced3d6ad5d0f81a80d889ccedc7136f1890c0
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2025-10-02 13:11:11 +0200

    fix: add BlockchainVerificationService to ApiModule providers

--- src/monitoringV2/api/api.module.ts
+++ src/monitoringV2/api/api.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { PrismaClientService } from '../prisma/client.service';
 import { AppConfigService } from '../../config/config.service';
+import { BlockchainVerificationService } from '../blockchain-verification.service';
+import { ProviderService } from '../provider.service';
 
 @Module({
 	controllers: [ApiController],
-	providers: [PrismaClientService, AppConfigService],
+	providers: [PrismaClientService, AppConfigService, ProviderService, BlockchainVerificationService],
 })
 export class ApiModule {}
